3c0699f91430504a2f3aa10c5f8b6c4bfa4fb481
Veronika <akulova.veronika17@gmail.com>
2021-01-27 16:36:18 +0700

    fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,18 @@
 window.addEventListener('DOMContentLoaded', function() {
     'use strict';
+
+    // Настройка реакции эл-та на активную кнопку
+
+    const carBtns = document.querySelectorAll('.goods__btn');
+    for (let btn of carBtns) {
+        btn.addEventListener('click', function() {
+            btn.classList.toggle('goods__btn_focus');
+            let parent = btn.closest('.goods__add').closest('.goods__item');
+            parent.classList.toggle('goods__item_focus');
+        });
+    }
+
+
     const dropBtns = document.querySelectorAll('.drop__btn'),
         dropMenu = document.querySelector('.drop__menu');
     
